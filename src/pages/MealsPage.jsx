15d7e756706f19972f793c1ad3bfacbb7ed49890
OyinDee <99656110+OyinDee@users.noreply.github.com>
2024-10-10 20:27:44 +0100

    Update MealsPage.jsx

--- src/pages/MealsPage.jsx
+++ src/pages/MealsPage.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { jwtDecode } from "jwt-decode";
-import { toast } from "react-toastify";
+import { toast, ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
 const Loader = () => (
