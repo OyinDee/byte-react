0fff5de02a13542846a1ce5cc76ebd86a9235a07
OyinDee <99656110+OyinDee@users.noreply.github.com>
2024-10-15 19:40:08 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,11 +12,15 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
-
+serviceWorkerRegistration.register({
+  onUpdate: registration => {
+    // Notify the user about the update
+    if (window.confirm("A new version of the app is available. Reload now?")) {
+      registration.waiting.postMessage({ type: 'SKIP_WAITING' });
+      window.location.reload();
+    }
+  },
+});
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
