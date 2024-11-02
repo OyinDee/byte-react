73ea17ca138972d44262be0436df1f928b63e5b7
OyinDee <99656110+OyinDee@users.noreply.github.com>
2024-11-02 08:33:13 +0100

    Update OrderHistory.jsx

--- src/pages/OrderHistory.jsx
+++ src/pages/OrderHistory.jsx
@@ -48,7 +48,7 @@ const OrderHistory = () => {
 
   useEffect(() => {
     fetchOrderHistory();
-  }, [page, fetchOrderHistory]);
+  }, [page]);
 
   const handleExpandClick = (orderId) => {
     setExpandedOrders((prevState) => ({
